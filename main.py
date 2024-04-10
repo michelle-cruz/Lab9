f2305f1aeb62389965304d88397750dd9446de49
ntrejo12 <naydelintrejo12@gmail.com>
2024-04-10 11:24:47 -0400

    Added decode function

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+def decode(encode_pass):
+    new_pass = []
+    for num in encode_pass:
+        int_num = int(num)
+        int_num -= 3
+        str_num = str(int_num)
+        new_pass.append(str_num)
+    decode_pass = ''.join(new_pass)
+    return decode_pass
+
 def main():
     while True:
         print("Menu")
@@ -20,7 +30,8 @@ def main():
             print('Your password has been encoded and stored!\n')
 
         if menu_option == 2:
-            print(f'The encoded password is {final_encode}, and the original password is {password}.\n')
+            new_pass = decode(final_encode)
+            print(f'The encoded password is {final_encode}, and the original password is {new_pass}.\n')
 
         if menu_option == 3:
             break
